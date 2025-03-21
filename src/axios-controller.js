efe328393af9dcb4342184af708e0e498c163a32
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-03-21 20:12:51 +0000

    modified:   src/axios-controller.js

--- src/axios-controller.js
+++ src/axios-controller.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-
 const instance = axios.create({
-  baseURL: "/api",
+  baseURL: window.location.origin + "/api",
+  withCredentials: true,
 });
-
 export default instance;
