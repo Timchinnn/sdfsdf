dfbd180547c44623962893dd2c17091f5f6881cf
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-03-22 11:19:03 +0000

    modified:   src/axios-controller.js

--- src/axios-controller.js
+++ src/axios-controller.js
@@ -3,9 +3,10 @@ const instance = axios.create({
     process.env.NODE_ENV === "production"
       ? "https://zoomayor.io/api"
       : "http://147.45.244.129:3000/api",
-  timeout: 15000,
+  timeout: 30000, // Увеличиваем таймаут до 30 секунд
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
   },
+  withCredentials: true, // Добавляем поддержку credentials
 });
