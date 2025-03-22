9129533e31981ae0a6b0d58332e7de86bec076e9
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-03-22 11:12:15 +0000

    modified:   src/axios-controller.js

--- src/axios-controller.js
+++ src/axios-controller.js
@@ -1,40 +1,11 @@
-import axios from "axios";
 const instance = axios.create({
   baseURL:
-    process.env.NODE_ENV === "production" ? "https://zoomayor.io/api" : "/api",
+    process.env.NODE_ENV === "production"
+      ? "https://zoomayor.io/api"
+      : "http://147.45.244.129:3000/api",
   timeout: 15000,
   headers: {
     "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
   },
 });
-// Добавляем перехватчик запросов
-instance.interceptors.request.use(
-  (config) => {
-    // Можно добавить токены или другие заголовки
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-// Добавляем перехватчик ответов
-instance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    if (error.code === "ECONNABORTED") {
-      // Обработка таймаута
-      console.error("Request timeout");
-    } else if (!error.response) {
-      // Обработка ошибки сети
-      console.error("Network error");
-    } else {
-      // Обработка ошибок сервера
-      console.error(`Server error: ${error.response.status}`);
-    }
-
-    return Promise.reject(error);
-  }
-);
-export default instance;
