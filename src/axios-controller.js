5c21ab0c54dcedd45132fdb58e4a7407c5bc6592
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-03-22 11:45:05 +0000

    modified:   src/axios-controller.js

--- src/axios-controller.js
+++ src/axios-controller.js
@@ -1,8 +1,16 @@
 import axios from "axios";
+// Настройка базового URL для axios
 const instance = axios.create({
-  baseURL: "https://zoomayor.io/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "https://zoomayor.io",
+  // можно добавить таймауты и другие опции, если необходимо
 });
+// Пример запроса с использованием настроенного экземпляра axios
+instance
+  .get("/api/endpoint")
+  .then((response) => {
+    console.log("Данные:", response.data);
+  })
+  .catch((error) => {
+    console.error("Ошибка:", error);
+  });
 export default instance;
