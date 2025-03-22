f47c142469fb369982c052c421f05e772b70f72b
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-03-22 12:05:18 +0000

    modified:   src/axios-controller.js

--- src/axios-controller.js
+++ src/axios-controller.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "https://zoomayor.io/api",
+  baseURL: "/api",
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json",
