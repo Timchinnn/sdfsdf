0502439f01c31b7321b9c12668b65d337ac6800c
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-03-22 07:30:00 +0000

    modified:   netlify.toml 	modified:   src/axios-controller.js

--- src/axios-controller.js
+++ src/axios-controller.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://api.zoomayor.io" // Продакшен API endpoint
+      ? "https://zoomayor.io" // Продакшен API endpoint
       : "/api", // Локальный API endpoint для разработки
 });
 export default instance;
