de47463ca4e64bf50b807bf387e2257ae4200676
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-03-23 10:41:33 +0000

    modified:   src/axios-controller.js

--- src/axios-controller.js
+++ src/axios-controller.js
@@ -3,8 +3,20 @@ const instance = axios.create({
   baseURL: "https://zoomayor.io/api",
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${localStorage.getItem("token")}`,
     "Access-Control-Allow-Origin": "*",
   },
 });
+// Добавляем интерцептор для запросов
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 export default instance;
