e3ad7e7cb60fd592a9448146eb7cb8a3fbf9dc7d
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-03-23 11:10:15 +0000

    modified:   src/axios-controller.js

--- src/axios-controller.js
+++ src/axios-controller.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "http://147.45.244.129:3000/api"
+      ? "https://147.45.244.129:3000/api"
       : "http://localhost:3000/api",
   withCredentials: true,
   timeout: 5000,
