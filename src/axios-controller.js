f6b6c50dc8289e85965b050455da033c6a9dbc82
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-03-23 09:56:05 +0000

    modified:   netlify.toml 	modified:   src/axios-controller.js

--- src/axios-controller.js
+++ src/axios-controller.js
@@ -1,7 +1,34 @@
 import axios from "axios";
-
 const instance = axios.create({
-  baseURL: "https://zoomayor.io:3000/api",
+  baseURL: "https://zoomayor.io/api",
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
 });
-
+// Add request interceptor
+instance.interceptors.request.use(
+  (config) => {
+    // Add any auth tokens if needed
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+// Add response interceptor
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response?.status === 404) {
+      console.error("Resource not found");
+    } else if (error.response?.status === 500) {
+      console.error("Server error");
+    }
+    return Promise.reject(error);
+  }
+);
 export default instance;
