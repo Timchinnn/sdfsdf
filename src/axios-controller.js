b64000d83005f64e8ea20810ba527163a1c6b275
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-03-23 10:36:12 +0000

    modified:   netlify.toml 	modified:   src/axios-controller.js

--- src/axios-controller.js
+++ src/axios-controller.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-
 const instance = axios.create({
-  baseURL: "/api",
+  baseURL: "https://zoomayor.io/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-
 export default instance;
