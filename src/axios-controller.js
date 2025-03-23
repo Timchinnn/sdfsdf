d4d215d2d59d8dec06008fab07c56995c54c30ec
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-03-23 10:52:30 +0000

    modified:   package.json 	modified:   src/axios-controller.js

--- src/axios-controller.js
+++ src/axios-controller.js
@@ -1,7 +1,12 @@
 import axios from "axios";
-
 const instance = axios.create({
-  baseURL: "/api",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://zoomayor.io/api"
+      : "http://localhost:3000/api",
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-
 export default instance;
