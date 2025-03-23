92d38cac9292937c645114480ff86517c8c56678
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-03-23 11:01:19 +0000

    modified:   src/axios-controller.js

--- src/axios-controller.js
+++ src/axios-controller.js
@@ -2,11 +2,9 @@ import axios from "axios";
 const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://zoomayor.io/api"
-      : "http://147.45.244.129:3000/api",
+      ? "http://147.45.244.129:3000/api"
+      : "http://localhost:3000/api",
+  withCredentials: true,
   timeout: 5000,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 export default instance;
