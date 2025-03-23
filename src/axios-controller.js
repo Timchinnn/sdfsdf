40d23b61c21c67cd12b6f7dc63ab5c9eab51bfb7
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-03-23 09:40:34 +0000

    modified:   src/axios-controller.js

--- src/axios-controller.js
+++ src/axios-controller.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://109.73.206.80:3000/api",
+  baseURL: "https://zoomayor.io:3000/api",
 });
 
 export default instance;
