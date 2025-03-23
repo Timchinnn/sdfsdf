ade47939c3d244a53a4e5e9f1e1b4419d8b4535a
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-03-23 09:34:38 +0000

    modified:   src/axios-controller.js

--- src/axios-controller.js
+++ src/axios-controller.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://147.45.244.129:3000/api",
+  baseURL: "https://147.45.244.129:3000/api",
 });
 
 export default instance;
