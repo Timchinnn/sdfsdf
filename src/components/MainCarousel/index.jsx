0bc89f6cc9b8d68ced34990855439fcca288079d
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-04-01 16:14:04 +0000

    modified:   src/components/MainCarousel/index.jsx

--- src/components/MainCarousel/index.jsx
+++ src/components/MainCarousel/index.jsx
@@ -204,7 +204,7 @@ const MainCarousel = ({
     setIsSwipeLocked(true); // Lock swiping when card is flipped
     setTimeout(() => {
       setIsSwipeLocked(false); // Unlock swiping after 15 seconds
-    }, 30900);
+    }, 900);
     const tg = window.Telegram.WebApp;
     const telegram_id = tg.initDataUnsafe?.user?.id;
 
