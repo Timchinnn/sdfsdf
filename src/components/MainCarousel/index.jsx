2bd78cae02a0a6bb57b6367fff9d3f41eca8337b
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-04-14 17:39:03 +0000

    modified:   src/components/MainCarousel/index.jsx

--- src/components/MainCarousel/index.jsx
+++ src/components/MainCarousel/index.jsx
@@ -242,7 +242,7 @@ const MainCarousel = ({
         setIsButtonLocked(false);
         setIsAnimating(false);
         setIsFlipped(false);
-      }, 2900);
+      }, 2300);
     }, ANIMATION_DURATION);
 
     const tg = window.Telegram.WebApp;
