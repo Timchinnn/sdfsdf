e00c0fa79a714c514d36bada25605401289dfb38
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-04-16 17:43:02 +0000

    modified:   src/pages/BonusPage/index.jsx

--- src/pages/BonusPage/index.jsx
+++ src/pages/BonusPage/index.jsx
@@ -1,7 +1,3 @@
-import React from "react";
-import routeBonus from "./routes";
-import MainSection from "components/MainSection";
-
 import React, { useState } from "react";
 import routeBonus from "./routes";
 import MainSection from "components/MainSection";
