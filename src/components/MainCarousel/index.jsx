2503e21fb9e68b7aa662df3d0a8a5e59a6e15879
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-04-19 12:06:55 +0000

    modified:   src/components/MainCarousel/index.jsx

--- src/components/MainCarousel/index.jsx
+++ src/components/MainCarousel/index.jsx
@@ -257,7 +257,7 @@ const MainCarousel = ({
     setIsSwipeLocked(true);
     setIsButtonLocked(true);
     // Set next open time to 5 seconds from now
-    const nextTime = Date.now() + 200; // 5 seconds cooldown
+    const nextTime = Date.now() + 5000; // 5 seconds cooldown
     setNextOpenTime((prev) => ({ ...prev, [index]: nextTime }));
     setTimeout(() => {
       setIsFlipped(true);
@@ -266,7 +266,7 @@ const MainCarousel = ({
         setIsButtonLocked(false);
         setIsAnimating(false);
         setIsFlipped(false);
-      }, 2000);
+      }, 3100);
     }, ANIMATION_DURATION);
 
     const tg = window.Telegram.WebApp;
