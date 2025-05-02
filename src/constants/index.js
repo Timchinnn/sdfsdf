92d138c9cdfe1faf78e2298aa809e5ea6b8981e2
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-05-02 12:13:38 +0000

    new file:   src/constants/index.js

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+import DefaultImg from "assets/img/default-card.png";
+import Style1CardBack from "assets/img/card1.png";
+import Style2CardBack from "assets/img/card2.png";
+export const cardBackStyles = {
+  default: { image: DefaultImg },
+  style1: { image: Style1CardBack },
+  style2: { image: Style2CardBack },
+};
