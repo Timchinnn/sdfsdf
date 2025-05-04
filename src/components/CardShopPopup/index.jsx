967e0b26130f0d7427305d6caad9816fa8e7705b
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-05-04 17:07:11 +0000

    modified:   src/components/CardShopPopup/index.jsx

--- src/components/CardShopPopup/index.jsx
+++ src/components/CardShopPopup/index.jsx
@@ -36,12 +36,10 @@ const CardShopPopup = (props) => {
         console.error("Telegram ID not found");
         return;
       }
-      const price = parseFloat(props.selectedPhoto.price).toFixed(2);
-
       const response = await axios.post("/shop/buy-card", {
         telegram_id,
-        card_id: props.selectedPhoto.id,
-        price: price,
+        card_id: props.selectedPhoto.card_id,
+        price: props.selectedPhoto.price,
       });
       if (response.data.success) {
         props.handleClosePopup();
