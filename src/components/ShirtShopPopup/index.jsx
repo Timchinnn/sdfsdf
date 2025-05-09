67fea8e6e63ccedcc0a9308f3843cb6a3d611dc8
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-05-09 08:39:58 +0000

    modified:   src/components/ShirtShopPopup/index.jsx

--- src/components/ShirtShopPopup/index.jsx
+++ src/components/ShirtShopPopup/index.jsx
@@ -58,7 +58,7 @@ const ShirtShopPopup = (props) => {
         <button
           type="button"
           className="shop-popup__close"
-          onClick={handleButtonClick}
+          onClick={props.handleClosePopup()}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
