190a02fa5462fb358d2ca877e1cb51d37b1395dc
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-05-28 19:31:10 +0300

    Update index.jsx

--- src/components/ShirtShopPopup/index.jsx
+++ src/components/ShirtShopPopup/index.jsx
@@ -57,6 +57,7 @@ const ShirtShopPopup = (props) => {
         if (onButtonClick) {
           onButtonClick();
         }
+        props.onShirtPurchased();
         // Показываем уведомление об успешной покупке
         tg.showPopup({
           title: "Успех!",
