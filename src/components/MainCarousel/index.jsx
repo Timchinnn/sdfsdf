154c00e515c60b2ef61c64795eb3260fe42d28e3
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-06-21 13:01:25 +0000

    modified:   src/components/MainCarousel/index.jsx

--- src/components/MainCarousel/index.jsx
+++ src/components/MainCarousel/index.jsx
@@ -318,7 +318,7 @@ const MainCarousel = ({
     setIsSwipeLocked(true);
     setIsButtonLocked(true);
 
-    const nextTime = Date.now() + 2500; // 5 seconds cooldown
+    const nextTime = Date.now() + 5500; // 5 seconds cooldown
     setNextOpenTime((prev) => ({ ...prev, [index]: nextTime }));
     setTimeout(() => {
       setIsFlipped(true);
