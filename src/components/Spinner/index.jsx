4db525102378c0f9aa4edf980a6cf244d258766f
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-06-28 14:13:36 +0000

    modified:   src/components/Spinner/index.jsx

--- src/components/Spinner/index.jsx
+++ src/components/Spinner/index.jsx
@@ -11,6 +11,7 @@ const Spinner = ({
   const spinnerStyle = {
     ...cssOverride,
     width: size,
+    marginTop: "62vw",
     height: size,
     borderColor: color,
     borderTopColor: "transparent",
