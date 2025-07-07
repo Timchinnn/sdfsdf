09b14689a5497a86dcd09947e8bf05a340807302
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-07-07 16:21:46 +0000

    modified:   src/pages/ShopPage/index.jsx

--- src/pages/ShopPage/index.jsx
+++ src/pages/ShopPage/index.jsx
@@ -332,7 +332,7 @@ const ShopPage = () => {
         
         // Translate card names
         for (const card of shopCards) {
-          translatedNames[`card_${card.id}`] = await translateText(card.name, language);
+          translatedNames[`card_${card.id}`] = await translateText(card.title, language);
         }
         
         // Translate shirt names
