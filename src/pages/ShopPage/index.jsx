b4e36a1b180b6db4e161cadac36dedf353712a74
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-07-07 16:54:43 +0000

    modified:   src/pages/ShopPage/index.jsx

--- src/pages/ShopPage/index.jsx
+++ src/pages/ShopPage/index.jsx
@@ -335,7 +335,7 @@ const ShopPage = () => {
           translatedNames[`card_${card.id}`] = await translateText(card.name, language);
         }
         
-        // Translate shirt namesl
+        // Translate shirt names
         for (const shirt of shirts) {
           translatedNames[`shirt_${shirt.id}`] = await translateText(shirt.name, language);
         }
@@ -711,7 +711,7 @@ const ShopPage = () => {
                                   </div>
                                   <div className="shop-list__content">
  <h3 className="shop-list__title">
-                                        {translations1[`card_${card.card_id}`] || card.title}
+                                        {translations1[`card_${card.id}`] || card.title}
                                     </h3>
                                     <div className="shop-list__price f-center">
                                       <img src={CoinIcon} alt="" />
