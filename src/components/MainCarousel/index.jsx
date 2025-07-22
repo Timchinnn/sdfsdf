42fb19d60786580dee3628ab6b4cbb2bf0f54993
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-07-22 16:52:23 +0000

    modified:   src/components/MainCarousel/index.jsx

--- src/components/MainCarousel/index.jsx
+++ src/components/MainCarousel/index.jsx
@@ -82,6 +82,7 @@ useEffect(() => {
         const userStatusResponse = await axios.get(`/admin/user/${telegram_id}`);
         const userStatusData = userStatusResponse.data;
         setUserStatus(userStatusData);
+        console.log(response.data)
       }
       // Show popup after 5 seconds if response time is slow
       setTimeout(() => {
