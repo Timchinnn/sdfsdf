a996d11342448be1d9073af9e2b1fe3316dc36ee
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-07-30 15:58:19 +0000

    modified:   src/pages/EditUser/index.jsx

--- src/pages/EditUser/index.jsx
+++ src/pages/EditUser/index.jsx
@@ -45,7 +45,7 @@ useEffect(() => {
     useEffect(() => {
     const fetchUserActions = async () => {
       try {
-        const response = await axios.get(`/user-actions/${id}`);
+        const response = await axios.get(`/user-actions/${user.id}`);
         setUserActions(response.data);
         console.log(response.data)
         console.log(id)
