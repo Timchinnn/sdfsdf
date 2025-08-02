8835fb6ce17a9b553b728b6eb4d21bfad7704670
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-08-02 17:39:35 +0000

    modified:   src/components/SettingsPopup/index.jsx 	modified:   src/components/SettingsPopup/index.jsx

--- src/components/SettingsPopup/index.jsx
+++ src/components/SettingsPopup/index.jsx
@@ -107,7 +107,7 @@ useEffect(() => {
 const cardBackName = purchasedShirts.find(
             (shirt) => shirt.image_url === cardBackStyleValue
           )?.name;
-          if (cardBackStyleValue === "default") {
+          else if (cardBackName || cardBackStyleValue === "default") {
             const translatedName = cardBackStyleValue === "default" ? 
               "Стандартная рубашка" : 
               await translateServerResponse(cardBackName);
