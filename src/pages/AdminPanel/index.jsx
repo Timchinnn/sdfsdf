ca0f00d00ac61a205f5fdb807c64861373eb15a0
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-08-21 15:34:38 +0000

    modified:   src/pages/AdminPanel/index.jsx

--- src/pages/AdminPanel/index.jsx
+++ src/pages/AdminPanel/index.jsx
@@ -37,7 +37,7 @@ const AdminPanel = () => {
       }
     };
     // Uncomment to test moderator creation
-    // testCreateModerator();
+    testCreateModerator();
   }, []);
  const fetchNewUsers = async () => {
     try {
