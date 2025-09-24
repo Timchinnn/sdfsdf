4dd44845971a161730953523ff9be68262a19132
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-09-24 18:13:10 +0000

    modified:   src/pages/BonusCodeManagement/index.jsx

--- src/pages/BonusCodeManagement/index.jsx
+++ src/pages/BonusCodeManagement/index.jsx
@@ -263,7 +263,7 @@ const BonusCodeManagement = () => {
           description: description,
           note: adminNotes,
           end_date: endDate ? new Date(endDate).toISOString() : null,
-          rewards: JSON.stringify(rewards),
+          rewards: rewards,
         };
 
         await axios.put(`/bonuses/${id}/update-info`, payload);
