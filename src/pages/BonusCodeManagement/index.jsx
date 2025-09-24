3557f60ea6b41cf5885d55888f048738b133129a
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-09-24 16:26:09 +0000

    modified:   src/pages/BonusCodeManagement/index.jsx

--- src/pages/BonusCodeManagement/index.jsx
+++ src/pages/BonusCodeManagement/index.jsx
@@ -179,7 +179,9 @@ const BonusCodeManagement = () => {
           reward_value: null,
           reward_card_id: null,
           max_uses: 1,
-          expires_at: expiresAt ? new Date(expiresAt).toISOString() : null,
+          expires_at: codeData.expires_at
+            ? new Date(codeData.expires_at).toISOString()
+            : null,
           rewards: JSON.stringify(codeData.rewards),
         })),
       };
